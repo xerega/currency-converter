9dc4fb51f931d748c83a061727845cbb429180a6
xerega <xerega.dev@gmail.com>
2024-04-10 21:23:09 +0300

    initialize converter feature

---
+++ src/app/features/converter/converter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-converter',
+  standalone: true,
+  imports: [],
+  templateUrl: './converter.component.html',
+  styleUrl: './converter.component.scss',
+})
+export class ConverterComponent {}
