ba01242d704328859c6239f730b3d43f6c1d2d36
xerega <xerega.dev@gmail.com>
2024-04-11 12:33:44 +0300

    apply httpclientmodule providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,12 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { TuiRootModule } from '@taiga-ui/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideAnimations(), importProvidersFrom(TuiRootModule)],
+  providers: [
+    provideAnimations(),
+    importProvidersFrom(TuiRootModule),
+    importProvidersFrom(HttpClientModule),
+  ],
 };
